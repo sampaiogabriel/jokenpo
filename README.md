95bc647a9cbf9d85de6fbb6c61903f839b2c288a
GabrielSampaio7 <gabriel.sampaio@viannasempre.com.br>
2021-11-06 11:43:35 -0300

    adicionando imagens no projeto, adicionado readme atualizado

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# jokenpo
+# Jokenpô
+
+<img src="src/images/image card back.png" align="center">
+
+<p>Pedra, papel e tesoura, também chamado em algumas regiões do Brasil de jokenpô (do japonês じゃんけんぽん, jankenpon), é um jogo de mãos recreativo e simples para duas ou mais pessoas, que não requer equipamentos nem habilidade. </p>
+
+<p>O jogo é frequentemente empregado como método de seleção (como na escolha de equipes para a prática desportiva, por exemplo), assim como lançar moedas, jogar dados, entre outros. No entanto, diferentemente desses métodos que se baseiam exclusivamente em sorte, pedra-papel-tesoura pode ser jogado com um pouco de habilidade. Principalmente se o jogo se estender por vários turnos com o mesmo jogador, este pode reconhecer e explorar a lógica do comportamento do adversário (perceber e anteceder as jogadas do adversário).</p>
+
+<small>Fonte: <a href="https://pt.wikipedia.org/wiki/Pedra,_papel_e_tesoura">Wikipédia</a></small>
+
+## Ficha Técnica
+
+Este projeto usa as tecnologias HTML, CSS e Javascript. 
+
+Caso queira contribuir, tenho algumas sugestões:
+
+    - Responsividade
+    - Salvar histórico de jogo em Localstorage
+
+## Imagens do Projeto
+
+<img src="./jokenpo.gif" align="center" alt="Jokenpo Gif">
+
+
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,199 @@
-var card = document.querySelector('.card');
-card.addEventListener('click', function () {
-    card.classList.toggle('is-flipped');
+const score = {
+    you_score: 0,
+    cpu_score: 0
+}
+
+// -- Placar
+
+const updateScore = (number_cpu, number_you) => {
+    const score_cpu = document.querySelector("#score_cpu");
+    const score_you = document.querySelector("#score_you");
+
+    score_cpu.innerText = score.cpu_score = number_cpu;
+    score_you.innerText = score.you_score = number_you;
+}
+
+const papel = document.querySelector('#papel');
+const pedra = document.querySelector('#pedra');
+const tesoura = document.querySelector('#tesoura');
+
+papel.addEventListener('click', () => {
+    play('papel');
 });
+
+pedra.addEventListener('click', () => {
+    play('pedra');
+});
+
+tesoura.addEventListener('click', () => {
+    play('tesoura');
+});
+
+/////////////////
+
+const playCpu = () => {
+    const value = Math.floor(Math.random() * (3 - 1) + 1);
+
+    switch (value) {
+        case 1:
+            return 'papel';
+            break;
+
+        case 2:
+            return 'pedra';
+            break;
+
+        case 3:
+            return 'tesoura'
+            break;
+    }
+}
+
+const compareResult = (you, cpu) => {
+    if (you === cpu) {
+        return 'empate';
+    }
+
+    if (you === 'pedra' && cpu === 'papel') {
+        return 'cpu';
+    }
+
+    if (you === 'papel' && cpu === 'tesoura') {
+        return 'cpu';
+    }
+
+    if (you === 'tesoura' && cpu === 'pedra') {
+        return 'cpu';
+    }
+
+    return 'you';
+}
+
+const addResultScore = (winner) => {
+    if (winner === 'cpu') {
+        score.cpu_score += 1;
+    }
+
+    if (winner === 'you') {
+        score.you_score += 1;
+    }
+
+    return score;
+}
+
+const updateScoreInfo = (winner) => {
+
+    const score_info = document.querySelector('#score_info');
+
+    if (winner === 'empate') {
+        score_info.innerText = 'Empate! 😐'
+    }
+
+    if (winner === 'cpu') {
+        score_info.innerText = 'Perdeu! 😞'
+    }
+
+    if (winner === 'you') {
+        score_info.innerText = 'Ganhou! 😍'
+    }
+
+}
+
+const cardBackground = (card, background) => {
+
+    if (background === 'pedra') {
+        card.classList.add('background-pedra');
+    } else if (background === 'papel') {
+        card.classList.add('background-papel');
+    } else if (background === 'tesoura') {
+        card.classList.add('background-tesoura');
+    }
+
+}
+
+const cardBackgroundReset = (card) => {
+    card.classList.remove('background-pedra');
+    card.classList.remove('background-papel');
+    card.classList.remove('background-tesoura');
+}
+
+const cardFlip = (play_you, play_cpu) => {
+    const game_cpu_move = document.querySelector('#game-cpu-move');
+    const game_you_move = document.querySelector('#game-you-move');
+
+    const card_cpu_face = document.querySelector('#game_cpu_card_face_back');
+    const card_you_face = document.querySelector('#game_you_card_face_back');
+
+    const card_cpu = document.querySelector('.card-cpu');
+    const card_you = document.querySelector('.card-you');
+
+    cardBackgroundReset(card_cpu_face);
+    cardBackgroundReset(card_you_face);
+
+    if (play_you === 'pedra') {
+        game_you_move.innerText = '✊'
+        cardBackground(card_you_face, play_you);
+    } else if (play_you === 'papel') {
+        game_you_move.innerText = '✋'
+        cardBackground(card_you_face, play_you);
+    } else if (play_you === 'tesoura') {
+        game_you_move.innerText = '✌️'
+        cardBackground(card_you_face, play_you);
+    }
+
+    if (play_cpu === 'pedra') {
+        game_cpu_move.innerText = '✊'
+        cardBackground(card_cpu_face, "pedra");
+    } else if (play_cpu === 'papel') {
+        game_cpu_move.innerText = '✋'
+        cardBackground(card_cpu_face, "papel");
+    } else if (play_cpu === 'tesoura') {
+        game_cpu_move.innerText = '✌️'
+        cardBackground(card_cpu_face, "tesoura");
+    }
+
+    card_cpu.classList.toggle('is-flipped');
+    card_you.classList.toggle('is-flipped');
+}
+
+
+const blockPlay = () => {
+    papel.removeEventListener('click', () => {}, false);
+}
+
+const resetFlipCard = () => {
+    const card_cpu = document.querySelector('.card-cpu');
+    const card_you = document.querySelector('.card-you');
+
+    card_cpu.classList.toggle('is-flipped');
+    card_you.classList.toggle('is-flipped');
+}
+
+const loadingCard = () => {
+
+    setTimeout(() => {
+        blockPlay();
+        resetFlipCard();
+    }, 2000)
+
+}
+
+const play = (play_you) => {
+    const play_cpu = playCpu();
+    const winner = compareResult(play_you, play_cpu);;
+
+    cardFlip(play_you, play_cpu);
+
+    loadingCard();
+
+    addResultScore(winner);
+    updateScore(score.cpu_score, score.you_score);
+    updateScoreInfo(winner);
+
+}
+
+
+
+
+
+
